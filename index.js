87e0426ed7ee90505aa3479598b485538a15d89d
eitofujita <fifatvgame@gmail.com>
2024-11-17 17:07:31 +0900

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-
-        document.getElementById('calculateBtn').addEventListener('click', function() 
-        {
-            const quantity = document.getElementById('quantity').value;
-            const productPrice = document.getElementById('product').value;
-            const totalPrice = quantity * productPrice;
-
-            document.getElementById('totalPrice').textContent = totalPrice;
-        })
